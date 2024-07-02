42b80b68f4d17c919897971e277168e60e232978
nailtonsantoszx <nailtonramos23@gmail.com>
2024-07-02 17:54:25 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,25 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const carouselContainer = document.querySelector('.carousel-container');
-    const carouselItems = document.querySelectorAll('.carousel-item');
-    const prevButton = document.querySelector('.carousel-button.prev');
-    const nextButton = document.querySelector('.carousel-button.next');
+    const addImageButtons = document.querySelectorAll('.add-image-btn');
     
-    let index = 0;
-
-    const showItem = (index) => {
-        const offset = -index * 25;  // Ajustado para mostrar 4 itens no carrossel
-        carouselContainer.style.transform = `translateX(${offset}%)`;
-    };
-
-    prevButton.addEventListener('click', () => {
-        index = (index > 0) ? index - 1 : carouselItems.length - 1;
-        showItem(index);
+    addImageButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const newImage = prompt('Insira o URL da nova imagem:');
+            if (newImage) {
+                const productImg = button.parentElement.querySelector('img');
+                productImg.src = newImage;
+            }
+        });
     });
-
-    nextButton.addEventListener('click', () => {
-        index = (index < carouselItems.length - 1) ? index + 1 : 0;
-        showItem(index);
-    });
-
-    showItem(index);
 });

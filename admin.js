042722697867a30ec222b2abc83a5e0e4e290689
LN-Modas <nailtonramos23@gmail.com>
2024-07-23 22:03:14 -0300

    Delete admin.js

--- admin.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Adiciona a classe 'admin' ao body se for um administrador
-    if (document.body.classList.contains('admin')) {
-        document.querySelectorAll('.admin-only').forEach(el => {
-            el.style.display = 'block';
-        });
-    }
-});
